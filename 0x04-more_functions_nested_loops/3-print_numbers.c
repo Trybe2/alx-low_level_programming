fa64b628fe1e25630fb5ffd0fa91900e2e14beeb
Cindy Ngatia <125447669+Trybe2@users.noreply.github.com>
2023-03-23 10:03:19 -0700

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,10 +4,9 @@
  */
 void print_numbers(void)
 {
-	int num;
+	int x;
 
-	for (num = 0; num <= 9; num++)
-		_putchar((num % 10) + '0');
-
-	_putchar('\n');
+	for (x = 0; x <= 9; x++)
+     _putchar(x + '0');
+     _putchar('\n');
 }
